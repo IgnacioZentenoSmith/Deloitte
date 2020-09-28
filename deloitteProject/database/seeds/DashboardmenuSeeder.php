04550fd94a905949c54167c410ad52c3c0e83eab
Ignacio Zenteno Smith <31903811+IgnacioZentenoSmith@users.noreply.github.com>
2020-09-28 00:47:58 -0300

    fix de sintaxis

--- deloitteProject/database/seeds/DashboardmenuSeeder.php
+++ deloitteProject/database/seeds/DashboardmenuSeeder.php
@@ -12,10 +12,8 @@ class MenusSeeder extends Seeder
     public function run()
     {
         DB::table('menus')->insert([
-            DB::table('menus')->insert([
-                ['menus_nombre'=>'Dashboard', 'menus_tipo'=>'Menu', 'menus_idPadre'=>null, 'menus_descripcion'=>'Acceso al Menú de Dashboard',],
-                ['menus_nombre'=>'Dashboard_index', 'menus_tipo'=>'Programa', 'menus_idPadre'=>10, 'menus_descripcion'=>'Acceso a ver los gráficos del Dashboard',],
-            ]);
+            ['menus_nombre'=>'Dashboard', 'menus_tipo'=>'Menu', 'menus_idPadre'=>null, 'menus_descripcion'=>'Acceso al Menú de Dashboard',],
+            ['menus_nombre'=>'Dashboard_index', 'menus_tipo'=>'Programa', 'menus_idPadre'=>10, 'menus_descripcion'=>'Acceso a ver los gráficos del Dashboard',],
         ]);
     }
 }
