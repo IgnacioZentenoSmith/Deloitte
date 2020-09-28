ef045bf9fc913d1616d59b70c96642c150fe3b3e
Ignacio Zenteno Smith <31903811+IgnacioZentenoSmith@users.noreply.github.com>
2020-09-28 00:47:03 -0300

    crear los contenedores de los graficps

--- deloitteProject/resources/views/dashboard/index.blade.php
+++ deloitteProject/resources/views/dashboard/index.blade.php
@@ -1,8 +1,24 @@
 @extends('dashboard.layout')
 @section('dashboardContent')
 <div class="row justify-content-center">
-    <div class="col-auto">
 
+    <div class="col-6">
+        <figure class="highcharts-figure">
+            <div id="barChart"></div>
+        </figure>
     </div>
+    <div class="col-6">
+        <figure class="highcharts-figure">
+            <div id="pieChart"></div>
+        </figure>
+    </div>
+    <div class="col-12">
+        <figure class="highcharts-figure">
+            <div id="areaChart"></div>
+        </figure>
+    </div>
+
+
+
 </div>
 @endsection
