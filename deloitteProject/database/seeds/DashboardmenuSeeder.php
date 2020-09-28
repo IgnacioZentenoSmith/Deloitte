5ee5582a90e3f7efc4cfb3d7d9c242bd879f180b
Ignacio Zenteno Smith <31903811+IgnacioZentenoSmith@users.noreply.github.com>
2020-09-28 00:47:22 -0300

    seeder de los permisos del dashboard

---
+++ deloitteProject/database/seeds/DashboardmenuSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class MenusSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('menus')->insert([
+            DB::table('menus')->insert([
+                ['menus_nombre'=>'Dashboard', 'menus_tipo'=>'Menu', 'menus_idPadre'=>null, 'menus_descripcion'=>'Acceso al Menú de Dashboard',],
+                ['menus_nombre'=>'Dashboard_index', 'menus_tipo'=>'Programa', 'menus_idPadre'=>10, 'menus_descripcion'=>'Acceso a ver los gráficos del Dashboard',],
+            ]);
+        ]);
+    }
+}
