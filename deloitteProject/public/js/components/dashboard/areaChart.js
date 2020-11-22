e39c4e96f9d56d5c6694a506c8b3fdb32ee095c7
Ignacio Zenteno Smith <31903811+IgnacioZentenoSmith@users.noreply.github.com>
2020-11-22 18:28:33 -0300

    graficos genericos

--- deloitteProject/public/js/components/dashboard/areaChart.js
+++ deloitteProject/public/js/components/dashboard/areaChart.js
@@ -1,50 +1,54 @@
-Highcharts.chart('areaChart', {
-    chart: {
-        type: 'area'
-    },
-    title: {
-        text: 'Acumulado de los dividendos retenidos y pagados durante el año 2019'
-    },
-    xAxis: {
-        categories: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
-    },
-    yAxis: {
-        title: {
-            text: 'Dividendos acumulados en millones de CLP'
+function areaChartConstructor(containerID, title_Text, xAxisCategories, yAxisTitle, areaData) {
+    //Grafico de torta genérico
+	$(containerID).chart = Highcharts.chart({
+        chart: {
+            type: 'area',
+            renderTo: containerID
         },
-        labels: {
-            formatter: function () {
-                return this.value / 1000 + 'k';
+        title: {
+            text: title_Text
+        },
+        xAxis: {
+            categories: xAxisCategories,
+        },
+        yAxis: {
+            title: {
+                text: yAxisTitle
+            },
+            labels: {
+                formatter: function () {
+                    return this.value / 1000 + 'k';
+                }
             }
-        }
-    },
-    tooltip: {
-        pointFormat: '{series.name}: <b>{point.y:,.0f} millones de CLP</b>'
-    },
-    plotOptions: {
-        area: {
-            marker: {
-                enabled: false,
-                symbol: 'circle',
-                radius: 2,
-                states: {
-                    hover: {
-                        enabled: true
+        },
+        tooltip: {
+            pointFormat: '{series.name}: <b>{point.y:,.0f}</b>'
+        },
+        plotOptions: {
+            area: {
+                marker: {
+                    enabled: false,
+                    symbol: 'circle',
+                    radius: 2,
+                    states: {
+                        hover: {
+                            enabled: true
+                        }
                     }
                 }
             }
-        }
-    },
-    series: [{
-        name: 'Dividendos retenidos',
-        data: [
-            297126, 271176, 238121, 221356, 191152, 151152, 141351, 135959, 106867, 99211,
-            69123, 56812
-        ]
-    }, {
-        name: 'Dividendos pagados',
-        data: [102874, 128824, 161879, 178644, 208848, 248848, 258649, 264041, 293133, 300789,
-            330877, 343188,
-        ]
-    }]
-});
+        },
+        series: [{
+            name: 'Dividendos retenidos',
+            data: [
+                297126, 271176, 238121, 221356, 191152, 151152, 141351, 135959, 106867, 99211,
+                69123, 56812
+            ]
+        }, {
+            name: 'Dividendos pagados',
+            data: [102874, 128824, 161879, 178644, 208848, 248848, 258649, 264041, 293133, 300789,
+                330877, 343188,
+            ]
+        }]
+    });
+}
--- deloitteProject/public/js/components/dashboard/barChart.js
+++ deloitteProject/public/js/components/dashboard/barChart.js
@@ -1,59 +1,54 @@
-Highcharts.chart('barChart', {
-    chart: {
-        type: 'bar'
-    },
-    title: {
-        text: 'Cantidad de socios con retención de dividendos'
-    },
-    subtitle: {
-        text: 'Período: Abril 2020'
-    },
-    xAxis: {
-        categories: ['Enero', 'Febrero', 'Marzo', 'Abril'],
-        title: {
-            text: null
-        }
-    },
-    yAxis: {
-        min: 0,
-        title: {
-            text: 'Cantidad de socios',
-            align: 'high'
+function barChartConstructor(containerID, xAxisCategories, yAxisTitle, title_Text, subTitle_Text, barData) {
+    //Grafico de barras genérico
+	$(containerID).chart = Highcharts.chart({
+        chart: {
+            type: 'bar',
+            renderTo: containerID
         },
-        labels: {
-            overflow: 'justify'
-        }
-    },
-    plotOptions: {
-        bar: {
-            dataLabels: {
-                enabled: true
+        title: {
+            text: title_Text
+        },
+        subtitle: {
+            text: subTitle_Text
+        },
+        xAxis: {
+            categories: xAxisCategories,
+            title: {
+                text: null
             }
-        }
-    },
-    legend: {
-        layout: 'vertical',
-        align: 'right',
-        verticalAlign: 'top',
-        x: -40,
-        y: 80,
-        floating: true,
-        borderWidth: 1,
-        backgroundColor:
-            Highcharts.defaultOptions.legend.backgroundColor || '#FFFFFF',
-        shadow: true
-    },
-    credits: {
-        enabled: false
-    },
-    series: [{
-        name: 'Retenido',
-        data: [238, 197, 131, 68]
-    }, {
-        name: 'No retenido',
-        data: [242, 291, 361, 427]
-    }, {
-        name: 'Sin información',
-        data: [20, 12, 8, 5]
-    }]
-});
+        },
+        yAxis: {
+            min: 0,
+            title: {
+                text: yAxisTitle,
+                align: 'high'
+            },
+            labels: {
+                overflow: 'justify'
+            }
+        },
+        plotOptions: {
+            bar: {
+                dataLabels: {
+                    enabled: true
+                }
+            }
+        },
+        legend: {
+            layout: 'vertical',
+            align: 'right',
+            verticalAlign: 'top',
+            x: -40,
+            y: 80,
+            floating: true,
+            borderWidth: 1,
+            backgroundColor:
+                Highcharts.defaultOptions.legend.backgroundColor || '#FFFFFF',
+            shadow: true
+        },
+        credits: {
+            enabled: false
+        },
+        series: [barData]
+    });
+}
