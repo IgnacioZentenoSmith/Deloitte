03550c09d31d7559ecae4f0d3abb2b445f0f743d
Ignacio Zenteno Smith <31903811+IgnacioZentenoSmith@users.noreply.github.com>
2020-11-22 18:28:07 -0300

    Update DashboardController.php

--- deloitteProject/app/Http/Controllers/DashboardController.php
+++ deloitteProject/app/Http/Controllers/DashboardController.php
@@ -8,6 +8,12 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 //Traer modelos
+use App\Socio;
+use App\Cuota;
+use App\Cumplimiento;
+use App\Pago;
+use Carbon\Carbon;
+
 use App\User;
 use App\Role;
 use App\Menu;
@@ -25,9 +31,70 @@ class DashboardController extends Controller
     public function index()
     {
         $permisos = $this->getPermisos();
-        return view('dashboard.index', compact('permisos'));
+        $socios = Socio::all();
+
+        return view('dashboard.index', compact('permisos', 'socios'));
     }
 
+    public function ajaxRequest(Request $request)
+    {
+        $request->validate([
+            'radioMetodoBusqueda'=>'required|string|max:255',
+        ]);
+        if ($request->radioMetodoBusqueda == 'cuota') {
+
+        } else if ($request->radioMetodoBusqueda == 'mes') {
+
+        } else if ($request->radioMetodoBusqueda == 'socio') {
+            //ID del socio desde el select box del filtro socios
+            $socioData = $this->getThisSocioData($request->selectSocio);
+            $socio = Socio::find($request->selectSocio);
+
+            $cumplimientosPorMes = collect([
+                'name' => 'Cumplimiento',
+                'data' => [],
+            ]);
+            $pagosPorMes = collect([
+                'name' => 'Pagos',
+                'data' => [],
+            ]);
+            $meses = [];
+            $montoRetenidoPorMes = [];
+            foreach ($socioData as $sdata) {
+                //Meses
+                array_push($meses, $sdata->pagos_fecha);
+                //Barras cumplimiento y sus pagos
+                $cumplimientosPorMes = $cumplimientosPorMes->mergeRecursive(['data' => (float)$sdata->cumplimientos_porcentaje]);
+                $pagosPorMes = $pagosPorMes->mergeRecursive(['data' => (int)$sdata->pagos_montoPagar]);
+                //Barras monto retenido por mes
+                array_push($montoRetenidoPorMes, $sdata->pagos_montoRetencion);
+            }
+            return response()->json(['nombreSocio' => $socio->socios_nombre, 'socioData' => $socioData, 'metodoBusqueda' => $request->radioMetodoBusqueda, 'barraCumplimientos' => $cumplimientosPorMes, 'barraPagos' => $pagosPorMes, 'categoriaMeses' => $meses]);
+
+        }
+        return response()->json(['Estoy funcionando']);
+    }
+
+    private function getThisSocioData($idSocio) {
+        $data = Cuota::where('cuotas.socios_id', $idSocio)
+        ->join('pagos', 'pagos.cuotas_id', '=', 'cuotas.id')
+        ->join('cumplimientos', function($subQuery) use ($idSocio) {
+            $subQuery->on('cumplimientos.cumplimientos_fecha', '=', 'pagos.pagos_fecha')
+            ->where('cumplimientos.socios_id', $idSocio);
+        })
+        ->get();
+        return $data;
+    }
+
+    private function getThisMonthData($mes) {
+        $data = [];
+        return $data;
+    }
+
+    private function getThisCuotaData($cuota) {
+        $data = [];
+        return $data;
+    }
     /**
      * Show the form for creating a new resource.
      *
