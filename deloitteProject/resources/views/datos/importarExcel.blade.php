224114fd6c37c8c0783c197158bf714565e9751f
Ignacio Zenteno Smith <31903811+IgnacioZentenoSmith@users.noreply.github.com>
2020-12-04 01:27:21 -0300

    Vista de eliminar e importar excels

---
+++ deloitteProject/resources/views/datos/importarExcel.blade.php
@@ -0,0 +1,26 @@
+@extends('datos.layout')
+@section('datosContent')
+
+
+<div class="row justify-content-center">
+    <div class="col-auto">
+        <form method="POST" id="excel" action="{{route('datos.postImportarExcel')}}" enctype="multipart/form-data">
+            @csrf
+            <div class="form-group">
+                <label for="file">Subir excel de datos</label>
+                <input type="file" class="form-control-file" id="file" name="file">
+            </div>
+
+            <div class="form-group row">
+                <div class="col-md-3">
+                    <button type="submit" class="btn btn-primary">
+                        Subir
+                    </button>
+                </div>
+            </div>
+        </form>
+
+    </div>
+</div>
+
+@endsection
