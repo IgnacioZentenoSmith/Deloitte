0ba0ae403ed09153a56afe5e4af09209ded15269
Ignacio Zenteno Smith <31903811+IgnacioZentenoSmith@users.noreply.github.com>
2020-12-04 01:27:53 -0300

    Vista de historiales de retenciones y pagos

---
+++ deloitteProject/resources/views/historiales/layout.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.app')
+@section('content')
+
+<div class="card shadow">
+  <div class="card-header">
+    <ul class="nav nav-tabs card-header-tabs">
+
+    @if(in_array(5, $permisos))
+      <li class="nav-item">
+        <a class="nav-link {{ (request()->is('historiales/retenciones')) ? 'active' : '' }}" href="{{route('historiales.retenciones')}}">Historial de retenciones</a>
+      </li>
+    @endif
+
+    @if(in_array(3, $permisos))
+      <li class="nav-item">
+        <a class="nav-link {{ (request()->is('historiales/pagos')) ? 'active' : '' }}" href="{{route('historiales.pagos')}}">Historial de pagos</a>
+      </li>
+    @endif
+
+    </ul>
+  </div>
+
+  <div class="card-body py-3 my-3">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-12">
+          @yield('historialesContent')
+        </div>
+      </div>
+    </div>
+
+
+  </div>
+</div>
+@endsection
