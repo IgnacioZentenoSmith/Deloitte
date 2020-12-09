5d6e7ad5d6c250d4de543f0e5da9ccd7e2e55ce3
Ignacio Zenteno Smith <31903811+IgnacioZentenoSmith@users.noreply.github.com>
2020-12-09 05:59:13 -0300

    Iconos y texto del home

--- deloitteProject/resources/views/home/index.blade.php
+++ deloitteProject/resources/views/home/index.blade.php
@@ -11,32 +11,48 @@
 
                     <div class="row">
 
-                        <div class="col-3 text-center">
+                        <div class="col-3 text-center my-2">
+                            <h6>Home</h6>
+                            <img src="{{asset('img/home/').'/'.'homeHouse.svg'}}" class="img-fluid"
+                                alt="Responsive image">
+                            <p>Vista de recepción del sistema, entrega una breve descripción de cada uno de los módulos
+                                existentes dentro de él.</p>
+                        </div>
+                        <div class="col-3 text-center my-2">
                             <h6>Admin</h6>
                             <img src="{{asset('img/home/').'/'.'homeAdmin.svg'}}" class="img-fluid"
                                 alt="Responsive image">
                             <p>Módulo encargado de la creación, eliminación y editado de los usuarios, tanto de sus
                                 datos como de sus permisos.</p>
                         </div>
-                        <div class="col-3 text-center">
+                        <div class="col-3 text-center my-2">
                             <h6>Datos</h6>
                             <img src="{{asset('img/home/').'/'.'homeData.svg'}}" class="img-fluid"
                                 alt="Responsive image">
                             <p>Módulo encargado de importar datos de los Excel y la eliminación de algún Excel en caso
                                 de ser necesario.</p>
                         </div>
-                        <div class="col-3 text-center">
+                        <div class="col-3 text-center my-2">
                             <h6>Historiales</h6>
                             <img src="{{asset('img/home/').'/'.'homeHistoriales.svg'}}" class="img-fluid"
                                 alt="Responsive image">
                             <p>Módulo encargado de mostrar el detalle de las retenciones y los pagos de los socios.</p>
                         </div>
-                        <div class="col-3 text-center">
+                        <div class="col-3 text-center my-2">
                             <h6>Dashboard</h6>
                             <img src="{{asset('img/home/').'/'.'homeDashboard.svg'}}" class="img-fluid"
                                 alt="Responsive image">
                             <p>Módulo encargado de mostrar de forma resumida a través de gráficos la información de las
                                 cuotas y de los socios.</p>
+                        </div>
+                        <div class="col-3 text-center my-2">
+                            <h6>Bitácoras</h6>
+                            <img src="{{asset('img/home/').'/'.'homeBitacora.svg'}}" class="img-fluid"
+                                alt="Responsive image">
+                            <p>Módulo encargado de mostrar, en base a los filtros especificados, todas las acciones
+                                importantes
+                                que han sido realizadas en el sistema.
+                            </p>
                         </div>
 
                     </div>
